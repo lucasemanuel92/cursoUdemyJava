d09326d01ded0f826475e6327cb5241c7c6afeab
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2022-06-27 15:56:34 -0300

    Add files via upload

---
+++ fundamentos/Import.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+// "ctrl + shift + o" organiza os imports
+// Import fica sempre fora da classe
+import java.util.Date;
+
+public class Import {
+
+	public static void main(String[] args) {
+		
+		
+		String s = "Bom dia";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		//JButton botao = new JButton();
+	}
+}
