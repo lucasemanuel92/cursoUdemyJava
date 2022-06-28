411d10775d02cc6e702ac9bb413b79ac729eb44a
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2022-06-28 15:41:05 -0300

    Add files via upload

---
+++ controle/DesafioIf.java
@@ -0,0 +1,19 @@
+package controle;
+
+public class DesafioIf {
+
+	public static void main(String[] args) {
+
+		double nota = 1.3; 
+		
+		// Se colocar o ; depois do if ele inicía uma nova linha
+		if(nota >= 9.0) {
+			System.out.println("Quadro de Honra");
+			System.out.println("Você é fera!");
+		}
+
+	}
+
+}
+
+// O desafio era corrigir o bug do sistema porque tinha um ; logo após o if
